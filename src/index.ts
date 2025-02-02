4c71cae229a1ecb24ad60e7ace821863205c6ca9
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-02-02 18:49:06 +0100

    Refactor package.json and tsconfig.json; enhance logging in index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT} 🚀`);
 });
